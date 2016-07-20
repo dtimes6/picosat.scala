549933efcce1a54db2d2acd5a85b01eb647b6b61
代鹏 <dtimes6@163.com>
2016-07-21 00:20:44 +0800

    Create SatSolver.scala

---
+++ SatSolver.scala
@@ -0,0 +1,18 @@
+package logic.solver
+
+object SatSolver {
+  var setup = false;
+  def env() = {
+    if (!setup) {
+      System.load("libpicosat.dylib");
+    }
+  }
+}
+
+class SatSolver {
+  SatSolver.env;
+  @native def init();
+  @native def reset();
+  @native def add_clause(lits:Array[Int]);
+  @native def solve():Array[Int];
+}
